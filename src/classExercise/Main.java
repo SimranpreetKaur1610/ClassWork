757a60074bdbc966d205d3d8605adfc0d31edd76
Simranpreetkaur <harsim610@gmail.com>
2021-03-07 16:08:14 -0500

    After sorting the array in ascending order

--- src/classExercise/Main.java
+++ src/classExercise/Main.java
@@ -10,10 +10,26 @@ public class Main {
         {
             System.out.print(x + "  ");
         }
+
         System.out.println("\nArray in reverse order:");
         for (int i = myArray.length - 1 ; i >= 0 ; i-- )
         {
             System.out.print(myArray[i] + "  ");
         }
+
+        System.out.println("\n After Sorting the array in Ascending order:");
+        for(int i = 0 ; i < myArray.length ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(myArray[i] < myArray[j]){
+                    int a = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = a;
+                }
+            }
+        }
+        for (int x : myArray) {
+            System.out.print(x + "  ");
+        }
+
     }
 }
