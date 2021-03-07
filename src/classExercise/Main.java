7c28ddea79ef60671e70a414cbd0eea0187f1b61
Simranpreetkaur <harsim610@gmail.com>
2021-03-07 16:02:50 -0500

    initailizing the array

--- src/classExercise/Main.java
+++ src/classExercise/Main.java
@@ -2,7 +2,13 @@ package classExercise;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int[] myArray = {67,99,34,90,27,10,33};
+        System.out.println("Array :");
+        for (int x : myArray)
+        {
+            System.out.print(x + "  ");
+        }
     }
 }
